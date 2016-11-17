bdf4eb9ea9d64740297b9f5f550454ee89a14179
Y.Akamatsu <E87797@jp.ibm.com>
2016-11-17 14:37:05 +0900

    VisualRecognition呼び出しで言語を日本語(ja)に設定

--- utils/context.js
+++ utils/context.js
@@ -35,7 +35,10 @@ var visualRecognitionCreds = appEnv.getServiceCreds(visualRecognitionName);
 var visualRecognition = watson.visual_recognition({
     api_key: visualRecognitionCreds.api_key,
     version: 'v3',
-    version_date: '2016-05-19'
+    version_date: '2016-05-19',
+    headers : {
+        'Accept-Language': 'ja'
+    }
 });
 exports.visualRecognition = visualRecognition;
 
